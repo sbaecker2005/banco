f6b4a7a9bc54720c45e341896749df3ffc52c35a
Matheus Concon <142353640+goldjkk@users.noreply.github.com>
2024-05-17 19:52:46 -0300

    Update banco.c

--- banco.c
+++ banco.c
@@ -1,1 +1,21 @@
+#include "cliente_banco.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
+void arquivo_clientes(struct Cliente *clientesbanco, int numClientes) {
+  FILE *arquivo = fopen("clientesbanco.bin", "wb");
+  if (arquivo == NULL) {
+    perror("Erro ao abrir o arquivo");
+    return;
+  }
+
+  for (int i = 0; i < numClientes; i++) {
+    fprintf(arquivo, "%s,%s,%d,%.2lf,%s\n", clientesbanco[i].nome,
+            clientesbanco[i].cpf, clientesbanco[i].tipo, clientesbanco[i].saldo,
+            clientesbanco[i].senha);
+  }
+
+  fclose(arquivo);
+}
